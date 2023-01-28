4cfd4c28a2520b48042d801acc37e92dcfd0229f
Cod3guy <ian.kipkorir@gmail.com>
2023-01-28 06:35:54 -0800

    My personal work

---
+++ auto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#mkdir 0x01-git;
+#cd 0x01-git;
+#echo "Git folder" > README.md;
+#git add .
+#git commit -m "adding 0x01-git folder"
+#git push
+#mkdir bash c js
+#cd c; touch c_is_fun.c;
+#cd ..;
+#cd js; touch main.js index.js; 
+#cd ..;
+#cd bash;
+#touch alx school;
+#cat >> alx <<EOL
+ #"#/bin/bash"
+ #"echo 'Alx'" 
+#EOL
+#cat >> school <<EOL
+ ##/bin/bash
+ #echo "school"
+#EOL
+#cd ..;
+#git add .;
+#git commit -m "Added new directories";
+#git push;
+#cd bash; touch 98;
+
